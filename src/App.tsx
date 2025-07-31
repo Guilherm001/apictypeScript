85abcf2071da6c3c67ef567289a010a67a9ef55f
Guilherme <guilherm.rodrigues@outlook.com>
2025-07-30 23:09:40 -0300

    feat: adicionado react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-
+import { BrowserRouter } from 'react-router-dom';
+import { AppRoutes } from './routes';
 
 
 
 export const App = () => {
   return (
     <div>
-       teste
+      <BrowserRouter>
+
+        <AppRoutes />        
+      
+      </BrowserRouter>
+       
     </div>
   );
 }
